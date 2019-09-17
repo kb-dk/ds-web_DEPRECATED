5645f183c7495351a2c4df0d28ece2a3c04c5288
jara <jara@kb.dk>
2019-09-17 10:58:11 +0200

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,23 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'never'
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': true
+      }
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint'
