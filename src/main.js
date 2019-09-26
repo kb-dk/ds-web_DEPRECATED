fb3f5e866db95f2723fc5684789a35eead483504
jara <jara@kb.dk>
2019-09-26 11:12:39 +0200

    Install BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
 
 import App from './App.vue'
 import router from './router/router'
 import { search } from './store/search-store'
 import './styles/main.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Vuex)
+Vue.use(BootstrapVue)
 
 export const store = new Vuex.Store({
   modules: {
