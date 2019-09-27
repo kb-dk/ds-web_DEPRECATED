c376c2c28d5a46a9880d2bff3d76f307bf1d9daa
jara <jara@kb.dk>
2019-09-27 13:22:36 +0200

    Restructure and top navbar of header.

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><small>Udlån</small></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><small>Åbningstider</small></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><small>Om Det Kgl. Bibliotek</small></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><small>In English</small></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><small>Login</small></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="home">
-    <Welcome />
+  <div>
+    <Header />
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4" />
+        <div class="col-md-8" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Welcome from '@/components/Welcome.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
-    Welcome
+    Header
   }
 }
 </script>
