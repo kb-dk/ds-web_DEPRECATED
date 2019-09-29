b4eed380389379d4a2d7992014ce5bb8e2512fdc
jara <jara@kb.dk>
2019-09-29 11:51:57 +0200

    Second navbar, logo, and basic search form.

---
+++ src/components/Search.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <form class="mx-2 my-auto d-inline w-100">
+       <div class="input-group">
+          <input type="text" class="form-control" placeholder="...">
+           <span class="input-group-append">
+           <button class="btn btn-outline-secondary" type="button">GO</button>
+           </span>
+        </div>
+    </form>
+  </nav>
+</template>
