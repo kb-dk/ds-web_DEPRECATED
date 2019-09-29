05cbe6c4acf4d7a0f5e587ca3d7932d56864e90d
jara <jara@kb.dk>
2019-09-29 20:11:24 +0200

    Style search input.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,36 @@
 <template>
   <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <form class="mx-2 my-auto d-inline w-100">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="...">
+      <div class="input-group search-group">
+        <span class="input-group-prepend">
+          <div class="dropdown">
+            <a
+              id="dropdownMenuLink"
+              class="btn btn-secondary dropdown-toggle border-right-0 border text-secondary bg-white shadow-none"
+              href="#"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false">
+              <small>Søg i billeder</small>
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </span>
+        <input
+          id="search-input"
+          class="form-control py-2 border-right-0 border-left-0 border rounded-0 shadow-none"
+          type="search"
+          placeholder="Søg">
         <span class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button">
+          <button
+            class="btn btn-outline-secondary border-left-0 border bg-white shadow-none"
+            type="button">
             <icon name="search" />
           </button>
         </span>
@@ -15,8 +41,8 @@
 
 <script>
 import Icon from 'vue-awesome/components/Icon'
-export default ({
+export default {
   name: 'Search',
   components: Icon
-})
+}
 </script>
