590e5a0eaa93dc7fd6c0c30405d870a5120507cb
jara <jara@kb.dk>
2019-10-07 10:19:23 +0200

    Make result component.

---
+++ src/components/Results.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-4" />
+      <div class="col-md-8">
+        {{ searchResult }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Results',
+  computed: {
+    ...mapState({
+      searchResult: state => state.search.all.searchResult
+    })
+  }
+}
+</script>
