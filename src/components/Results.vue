743a62e110a4f23ba76f89f5b8c303039f0c1c27
jara <jara@kb.dk>
2019-10-07 13:52:06 +0200

    Pretty print json.

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -4,7 +4,9 @@
     <div class="row">
       <div class="col-md-4" />
       <div class="col-md-8">
-        {{ searchResult }}
+        <vue-json-pretty
+          :path="'res'"
+          :data="searchResult" />
       </div>
     </div>
   </div>
@@ -13,9 +15,13 @@
 <script>
 
 import { mapState } from 'vuex'
+import VueJsonPretty from 'vue-json-pretty'
 
 export default {
   name: 'Results',
+  components: {
+    VueJsonPretty
+  },
   computed: {
     ...mapState({
       searchResult: state => state.search.all.searchResult
