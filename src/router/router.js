b5955d6f83e1461e1b71022e7256dd0ea7020873
jara <jara@kb.dk>
2019-10-21 14:11:43 +0200

    Create basic view.

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '../views/Home.vue'
+import View from '../views/View.vue'
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/view/:id',
+      name: 'view',
+      component: View
     }
   ]
 })
