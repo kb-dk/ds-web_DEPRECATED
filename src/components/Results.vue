f99bf242d2cd7ec14a0054956066f51114fa6deb
jara <jara@kb.dk>
2019-11-08 14:30:54 +0100

    Results now with filters and result count.

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,25 +1,57 @@
 <template>
-  <div class="container-fluid">
-    <div v-if="searchResult" class="row results">
-      <div class="col-4">
-        <vue-json-pretty
-          :path="'res'"
-          :data="searchResult"
-          :highlight-mouseover-node="true" />
-      </div>
-      <div class="col-8">
-        <div class="card-columns">
-          <div v-for="result in searchResult.response.docs"
-               :key="result.id"
-               class="card mb-4">
-            <router-link :to="`/view/${result.id}`">
-              <img :src="getImage()" class="card-image-top img-fluid">
-            </router-link>
+  <main>
+    <div class="divider theme-color top-left" />
+    <div class="rdl-grid-container-wrapper position-relative" v-if="searchResult">
+      <div class="inner-content grid-container">
+        <div class="grid-x grid-margin-x grid-padding-y">
+          <div class="cell small-12">
+            <h2 class="h1">
+              {{ searchResult.response.docs.length }} resultater på &#039;{{ searchResult.responseHeader.params.q }}&#039;
+            </h2>
+
+            <div class="rdl-filter-btn-group position-relative">
+              <div class="grid-x grid-margin-x">
+                <div class="small-12 cell">
+                  <a href="#filter=access"
+                     class="filter-btn filter-btn">{{ $t('filters.access') | capitalize({ onlyFirstLetter: true }) }}</a>
+                  <a href="#filter=year"
+                     class="filter-btn filter-btn">{{ $t('filters.year') | capitalize({ onlyFirstLetter: true }) }}</a>
+                  <a href="#filter=locality"
+                     class="filter-btn filter-btn">{{ $t('filters.locality') | capitalize({ onlyFirstLetter: true }) }}</a>
+                  <div class="show
+-more show-for-small-only">
+                    <span class="more-text">Vis flere filtre</span>
+                    <span class="less-text">Vis færre filtre</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+    <div class="container-fluid">
+      <div v-if="searchResult" class="row results">
+        <div class="col-4">
+          <vue-json-pretty
+            :path="'res'"
+            :data="searchResult"
+            :highlight-mouseover-node="true" />
+        </div>
+        <div class="col-8">
+          <div class="card-columns">
+            <div v-for="result in searchResult.response.docs"
+                 :key="result.id"
+                 class="card mb-4">
+              <router-link :to="`/view/${result.id}`">
+                <img :src="getImage()" class="card-image-top img-fluid">
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
