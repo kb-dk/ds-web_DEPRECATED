73a2dd584a63faa3e24bed1bd789f2bdd3e07c61
jara <jara@kb.dk>
2019-11-11 14:58:37 +0100

    Masonry card columns.

--- src/components/CardColumns.vue
+++ src/components/CardColumns.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="grid-container">
-    <div class="grid-x grid-margin-x small-up-2 medium-up-3">
-      <div v-for="result in searchResult.response.docs"
-           :key="result.id"
-           class="cell">
-        <div class="card">
-          <router-link :to="`/view/${result.id}`">
-            <img :src="getImage()" class="card-image-top img-fluid">
-          </router-link>
-        </div>
-      </div>
+  <div class="masonry-css">
+    <div v-for="result in searchResult.response.docs"
+         :key="result.id"
+         class="masonry-css-item">
+      <router-link :to="`/view/${result.id}`">
+        <img :src="getImage()" class="card-image-top img-fluid">
+      </router-link>
     </div>
   </div>
 </template>
