b235de3d186608b2c16fd412673c0548084b3323
jara <jara@kb.dk>
2019-11-21 12:57:26 +0100

    Show image.

--- src/services/search-service.js
+++ src/services/search-service.js
@@ -1,12 +1,21 @@
 import axios from 'axios'
 
 export const searchService = {
-  search
+  search, lookup
 }
 
 function search (params) {
   const url = '/search-api/search/solr/ds/select?q=' + params
 
+  return axios.get(url).then(response => {
+    return response.data
+  }).catch(error => {
+    return Promise.reject(error)
+  })
+}
+
+function lookup (params) {
+  const url = '/search-api/search/solr/ds/get?ids=' + params
   return axios.get(url).then(response => {
     return response.data
   }).catch(error => {
