36ea1413c63b0dbda9415ff1d6ca3b008764446e
jara <jara@kb.dk>
2019-11-25 11:22:22 +0100

    PatternLab splitting up.

---
+++ src/components/Aside.vue
@@ -0,0 +1,12 @@
+<template>
+  <aside>
+    <ul>
+      <li>
+        <a @click="$router.go(-1)">
+          <i class="icon zmdi-chevron-left icon-small" />
+          Tilbage til oversigten
+        </a>
+      </li>
+    </ul>
+  </aside>
+</template>
---
+++ src/components/ProductHeading.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="rdl-page-intro  position-relative">
+  <div class="content-header">
+    <div class="grid-x grid-margin-x">
+      <div class="cell medium-8">
+        <h1>Page intro Heading</h1>
+      </div>
+      <div class="cell medium-4">
+        <span class="rdl-top-container-buttons hide-for-small-only">
+          <button aria-label="Increase Font">
+            <i class="icon zmdi-format-size icon-medium"></i>
+          </button>
+          <a href=""><i class="icon zmdi-print icon-medium"></i></a>
+          <a href=""><i class="icon zmdi-share icon-medium"></i></a>
+        </span>
+      </div>
+      <div class="cell small-12 large-8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda at fugiat deleniti totam? Minus omnis officia maxime perferendis delectus dolorem dignissimos quisquam! Illum architecto voluptas ea officiis! Distinctio, cupiditate quisquam.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
