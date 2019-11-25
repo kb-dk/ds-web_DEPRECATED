159fdd3da0dc41d15fa0f672e29cf75d39f4109d
jara <jara@kb.dk>
2019-11-25 11:22:22 +0100

    PatternLab splitting up.

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,31 +1,65 @@
 <template>
   <div>
     <Header />
-    <a href="#" @click="$router.go(-1)">&lt; Tilbage til søgningen</a>
+    <main>
 
-    <div class="rdl-page-image position-relative">
-      <div class="inner-content grid-container">
-        <div class="grid-x grid-margin-x grid-padding-y">
-          <div class="cell small-12">
-            <figure v-if="imageResult" class="rdl-figure margin-bottom page-image">
-              <img :src="imageResult.response.docs[0].image_full" alt="">
-              <span class="rdl-credits">Foto: Jens Jensen</span>
-            </figure>
+      <div class="rdl-bread-crumb-wrapper position-relative  show-for-large">
+        <div class="inner-content grid-container">
+          <div class="grid-x grid-margin-x">
+            <div class="hide-for-small-only cell small-12">
+              <div class="bread-crumb">
+                <i class="icon zmdi-home icon-xsmall"></i>
+                <a href="/">Forside</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div class="divider theme-color top-left"></div>
+
+      <div class="rdl-top-container position-relative">
+        <div class="inner-content">
+          <div class="grid-x">
+            <div class="cell small-12"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="rdl-grid-container-wrapper  position-relative">
+        <div class="inner-content grid-container">
+          <div class="grid-x grid-margin-x grid-padding-y">
+            <div class="cell show-for-large large-3 aside">
+              <Aside />
+            </div>
+            <div class="cell small-12 large-9 large-offset-3">
+              <div class="grid-x grid-margin-x grid-padding-y">
+                <div class="small-12 medium-8 cell">
+                  <ProductHeading />
+                </div>
+                <div class="small-12 medium-4 cell right-col">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Aside from '@/components/Aside.vue'
+import ProductHeading from '@/components/ProductHeading.vue'
 import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'ProductPage',
   components: {
-    Header
+    Header,
+    Aside,
+    ProductHeading
   },
   computed: {
     ...mapState({
