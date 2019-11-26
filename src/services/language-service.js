b35316a0eba971458b1256dcfdba699d7eccfa2c
jt <jt@kb.dk>
2019-11-26 15:17:00 +0100

    Refactored service call, refactored lang store to use new service call, some small adjustments and cleanup to language handling

--- src/services/language-service.js
+++ src/services/language-service.js
@@ -4,10 +4,10 @@ export const languageService = {
   setLanguage
 }
 
-async function setLanguage (params) {
-  let url = `${process.env.BASE_URL}locale/${params}.json`
-  // We need to now the right context before we can hit the right path
+function setLanguage (params) {
+  const url = `${process.env.BASE_URL}locale/${params}.json`
   return axios.get(url).then(response => {
+    console.log('axios resp', response)
     return response.data
   }).catch(error => {
     return Promise.reject(error)
