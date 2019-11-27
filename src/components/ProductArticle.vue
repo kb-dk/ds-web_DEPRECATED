c34f630b2bb53465afcc3d8b0c0a57ac6e6c1b51
jara <jara@kb.dk>
2019-11-27 13:52:39 +0100

    Insert footer and full post product page.

--- src/components/ProductArticle.vue
+++ src/components/ProductArticle.vue
@@ -1,27 +1,18 @@
 <template>
-  <div class="grid-x grid-margin-x grid-padding-y">
-    <div class="small-12 medium-8 cell">
-      <div class="rdl-page-image position-relative">
-        <div class="inner-content grid-container">
-          <div class="grid-x grid-margin-x grid-padding-y">
-            <div class="cell small-12">
-              <figure v-if="imageResult" class="rdl-figure margin-bottom page-image">
-                <img :src="imageResult.response.docs[0].image_preview" alt="">
-                <span class="rdl-credits">Foto: Jens Jensen</span>
-              </figure>
-              <p>
-                Dato: {{ imageResult.response.docs[0].datetime }}
-              </p>
-              <p>
-                keywords: {{ imageResult.response.docs[0].keyword[0] }}
-              </p>
-            </div>
-          </div>
-        </div>
+  <article>
+    <figure v-if="imageResult" class="rdl-figure article-center">
+      <div class="position-relative">
+        <img :src="imageResult.response.docs[0].image_preview" alt="">
+        <span class="rdl-credits">Foto: Jens Jensen</span>
       </div>
-    </div>
-    <div class="small-12 medium-4 cell right-col" />
-  </div>
+    </figure>
+    <p>
+      Dato: {{ imageResult.response.docs[0].datetime }}
+    </p>
+    <p>
+      keywords: {{ imageResult.response.docs[0].keyword[0] }}
+    </p>
+  </article>
 </template>
 
 <script>
